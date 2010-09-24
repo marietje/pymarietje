9aa5b178123d43dce975094ba766164bd4313f42
Bas Westerbaan <bas@westerbaan.name>
2010-09-24 18:43:21 +0200

    Add setup and prerequisites
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from get_git_version import get_git_version
+
+setup(name='maried',
+      version=get_git_version(),
+      description='Curses client for MarietjeD music daemon',
+      author='Bas Westerbaan',
+      author_email='bas@westerbaan.name',
+      url='http://github.com/bwesterb/pymarietje/',
+      packages=['pymarietje'],
+      package_dir={'pymarietje': 'src'},
+      install_requires = ['docutils>=0.3',
+			  'pyyaml>=3.00'],
+      entry_points = {
+	      'console_scripts': [
+		      'upload-to-marietje = pymarietje.main.upload:main',
+		      'marietje = pymarietje.cursesui:main',
+	      ]
+      )
